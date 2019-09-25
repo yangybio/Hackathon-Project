28e9a76424deab24a9d39cd9b25f5c4a32f9348b
Chaoyu <cyxiang@students.cs.ubc.ca>
2019-09-25 16:16:38 -0700

    declarative 3

--- src/test/modelTest/ItemListTest.java
+++ src/test/modelTest/ItemListTest.java
@@ -4,18 +4,38 @@ import model.Item;
 import model.ItemList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ItemListTest {
     private ItemList testList;
+
     @BeforeEach
-    public void runbefore(){
+    public void runbefore() {
         testList = new ItemList();
     }
+
     @Test
-    public void testInsertItem(){
+    public void testInsertItem() {
         Item addItem = new Item();
         testList.insert(addItem);
-        assertEquals(1,testList.size());
+        assertEquals(1, testList.size());
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(0, testList.size());
+        Item addItem = new Item();
+        int number = 10;
+        for (int i = 0; i < number; i++) {
+            testList.insert(addItem);
+        }
+        assertEquals(number, testList.size());
+    }
+
+    @Test
+    public void testGetItemList() {
+        ItemList newItemList = new ItemList();
+        assertEquals(newItemList.getItemList(), testList.getItemList());
     }
 }
