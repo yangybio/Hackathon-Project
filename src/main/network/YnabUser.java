b5c95e6119626ef176fce64b1ba93a1bf93fd6fb
Chaoyu <cyxiang@students.cs.ubc.ca>
2019-11-12 19:20:33 -0800

    deliverable 10

--- src/main/network/YnabUser.java
+++ src/main/network/YnabUser.java
@@ -10,7 +10,6 @@ import ynab.client.invoker.Configuration;
 import ynab.client.invoker.auth.ApiKeyAuth;
 import ynab.client.model.AccountsResponse;
 
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
@@ -23,6 +22,7 @@ public class YnabUser {
         token = "9d712bd3f769140d51a3c0c77dcc5646234ac2691de1c600c71d6eec05ff7e61";
         accountID = "b79ebeeb-6981-49e0-b020-2cef9ffb779d";
     }
+
     public double getCreditBlance() {
         double money = 0.0;
         ApiClient user = Configuration.getDefaultApiClient();
