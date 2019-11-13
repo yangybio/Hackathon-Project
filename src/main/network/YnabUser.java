9dfa44ecfada5763762117b241fa071258c3c9ab
Chaoyu <cyxiang@students.cs.ubc.ca>
2019-11-13 12:36:09 -0800

    deliverable 10

--- src/main/network/YnabUser.java
+++ src/main/network/YnabUser.java
@@ -2,7 +2,6 @@ package network;
 
 import model.DailyAddedItem;
 import model.Item;
-import model.exception.MoneyException;
 import ynab.client.api.AccountsApi;
 import ynab.client.invoker.ApiClient;
 import ynab.client.invoker.ApiException;
@@ -13,6 +12,8 @@ import ynab.client.model.AccountsResponse;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class YnabUser {
     private String token;
@@ -23,7 +24,7 @@ public class YnabUser {
         accountID = "b79ebeeb-6981-49e0-b020-2cef9ffb779d";
     }
 
-    public double getCreditBlance() {
+    public double getCreditBalance() {
         double money = 0.0;
         ApiClient user = Configuration.getDefaultApiClient();
         ApiKeyAuth bearer = (ApiKeyAuth) user.getAuthentication("bearer");
@@ -35,8 +36,7 @@ public class YnabUser {
         UUID budgetId = UUID.fromString(accountID); //The ID of the Budget.
         try {
             AccountsResponse result = account.getAccounts(budgetId);
-            money = result.getData().getAccounts().get(0).getBalance().doubleValue();
-            money = (-1 * money) / 1000.00;
+            money = this.getMoney(result);
 
         } catch (ApiException e) {
             System.err.println("Exception when getting accounts");
@@ -45,12 +45,27 @@ public class YnabUser {
         return money;
     }
 
-    public Item newCreditItem() throws MoneyException {
+    private double getMoney(AccountsResponse a) {
+        double money = 0.0;
+        String result = a.toString();
+        String pattern = "(balance: )(.+)((\\d)*)";
+        Pattern p = Pattern.compile(pattern);
+        String line = result.toString();
+        Matcher m = p.matcher(line);
+        if (m.find()) {
+            String s = m.group(0).substring(8);
+            money = Double.parseDouble(s);
+            money = ((-1) * money) / 1000.0;
+        }
+        return money;
+    }
+
+    public Item newCreditItem() {
         Item newItem;
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDateTime now = LocalDateTime.now();
         String date = dtf.format(now);
-        newItem = new DailyAddedItem(date, "RBC_Credit_Card", this.getCreditBlance());
+        newItem = new DailyAddedItem(date, "RBC_Credit_Card", this.getCreditBalance());
         return newItem;
     }
 }
